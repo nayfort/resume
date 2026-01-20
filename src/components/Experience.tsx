3a57718b1b9863a19f289dd0e950a91f0c16b73b
nayfort <mitrofanovvladislav2004@gmail.com>
2026-01-20 03:13:31 +0100

    refactor: frontend title

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,7 +9,7 @@ const Experience = () => {
     <div className="experienceSection">
       <div className="expBlockTitle">{t('experience')}:</div>
 
-      <Card title="BetTV (July 2023 / July 2024 - Junior Front-end Developer)" className='experienceCard'>
+      <Card title="BetTV (July 2023 / July 2024 - Junior Frontend Developer)" className='experienceCard'>
         <p className="expText">
           <b>Responsibilities & Contributions:</b>
           <br/>
@@ -69,7 +69,7 @@ const Experience = () => {
 
       <Divider />
 
-      <Card title="NerdLab (March 2022 / July 2023 - Intern Front-end Developer)" className='experienceCard'>
+      <Card title="NerdLab (March 2022 / July 2023 - Intern Frontend Developer)" className='experienceCard'>
         <p className="expText">
           <b>Responsibilities & Contributions:</b>
           <br/>
